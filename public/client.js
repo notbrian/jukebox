c9b291e6f76a686e3836a8f0667d0d3df830aeb0
Brian Nguyen <nguyen.brian70@gmail.com>
2018-01-25 21:49:02 -0500

    Fixed double click bug

--- public/client.js
+++ public/client.js
@@ -1,6 +1,7 @@
 var spotifyApi = new SpotifyWebApi();
 let user
 let playlistID
+let deviceID
 
 window.onload = function() {
   spotifyApi.setAccessToken(localStorage.getItem("spotifyToken"));
@@ -15,12 +16,12 @@ window.onload = function() {
       document.querySelector("#signout-button").style.display = "none"
     }
 
-  }
-)
+  })
 
   document.querySelectorAll('.button').forEach((element) => {
     element.addEventListener('click', changeGenre)
   })
+  deviceID = localStorage.getItem("deviceID")
 }
 document.querySelector('#playPause').addEventListener('click', sendOptions)
 document.querySelector('#signout-button').addEventListener('click', signOut)
@@ -39,8 +40,6 @@ function signOut() {
   location.reload();
 }
 
-function playPlaylist() {
-}
 
 function sendOptions(e) {
   // spotifyApi.getMyCurrentPlaybackState({}, function(err, res) {
@@ -49,58 +48,74 @@ function sendOptions(e) {
   //   } else if(res.is_playing == false && localStorage.getItem("changed") == "false") {
   //     spotifyApi.play({})
   //   } else {
-        let data;
-        const genre = localStorage.getItem('genreSelect')
-        const date = document.querySelector("#date").value
-        data = JSON.stringify([genre, date])
+  let data;
+  const genre = localStorage.getItem('genreSelect')
+  const date = document.querySelector("#date").value
+  data = JSON.stringify([genre, date])
 
-        var request = new XMLHttpRequest();
-        request.open('POST', "http://localhost:3000", true);
-        request.setRequestHeader("Content-type", "text/plain");
-        request.send(data);
+  var request = new XMLHttpRequest();
+  request.open('POST', "http://localhost:3000", true);
+  request.setRequestHeader("Content-type", "text/plain");
+  request.send(data);
 
-        request.onreadystatechange = function() {
-          if (request.readyState === 4) {
-            var songData = JSON.parse(request.response);
+  request.onreadystatechange = function() {
+    if (request.readyState === 4) {
+      var songData = JSON.parse(request.response);
 
-            var reformattedData = songData.map((element) => {
-              if (element.artist.includes("Featuring")) {
-                let artist = element.artist.split("Featuring")[0]
-                console.log(artist)
-                return `${element.title} ${artist}`
+      var reformattedData = songData.map((element) => {
+        if (element.artist.includes("Featuring")) {
+          let artist = element.artist.split("Featuring")[0]
+          return `${element.title} ${artist}`
 
-              } else {
-              return `${element.title} ${element.artist}`
-            }})
-
-            spotifyApi.createPlaylist(user.id, {
-              name: `${date} - ${genre}`
-            }).then(function(data) {
-              playlistID = data.id
-              localStorage.setItem('playlistURI', data.uri)
-              console.log(data.uri)
-
-
-              reformattedData.forEach(function(song) {
-                spotifyApi.searchTracks(song).then((data) => {
-                  spotifyApi.addTracksToPlaylist(user.id, playlistID, [data.tracks.items["0"].uri], function(err) {})
-                })
-              })
-
-
-            }).catch(function(err) {
-              console.log(err)
-            })
-
-          }
+        } else {
+          return `${element.title} ${element.artist}`
         }
-        setTimeout(function() {
-          spotifyApi.play({context_uri: localStorage.getItem('playlistURI'), device_id: localStorage.getItem('deviceID')})
-          localStorage.setItem("changed", "false")
-        }, 2000)
+      })
+
+      spotifyApi.createPlaylist(user.id, {
+        name: `${date} - ${genre}`
+      }).then(function(data) {
+        playlistID = data.id
+        localStorage.setItem('playlistURI', data.uri)
+        console.log(data.uri)
+
+
+        reformattedData.forEach(function(song) {
+          return new Promise((resolve) => {
+            spotifyApi.searchTracks(song).then((data) => {
+              spotifyApi.addTracksToPlaylist(user.id, playlistID, [data.tracks.items["0"].uri], function(err, data) {})
+            })
+          })
+        })
+
+        Promise.all(reformattedData).then(() => {
+          console.log(data.uri, deviceID)
+          setTimeout(function() {
+            spotifyApi.play({
+              context_uri: data.uri,
+              device_id: deviceID
+            })
+          }, 500)
+
+        });
+
+
+
+      }).catch(function(err) {
+        console.log(err)
+      })
 
     }
-  // })
+  }
+  // setTimeout(function() {
+  //   console.log(localStorage.getItem('playlistURI'))
+  //   console.log('playing playlsoit fam')
+  //   spotifyApi.play({context_uri: localStorage.getItem('playlistURI'), device_id: localStorage.getItem('deviceID')})
+  //   localStorage.setItem("changed", "false")
+  // }, 1500)
+
+}
+// })
 
 
 
